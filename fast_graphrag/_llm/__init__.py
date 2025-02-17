4eacfe86439ac095e16afdfb5b9a50677dadde96
FireKahuna <849166+TheFireKahuna@users.noreply.github.com>
2025-02-18 00:14:37 +1000

    Add Google Genai LLM class for Gemini
    Supports both Vertex and Gemini LLM endpoints, allowing the use of Flash 2.0 using the newer and native python-genai SDK, complete with structured JSON support. Includes default rate limits for the Gemini Developer AI and tuned to the highly rate-limited batch-embeddings API, as well as its own local tokenizer for token counting.
    
    Genai uses an internal retry mechanism atop a surrounding retry for random structured JSON errors and rare no-response returns, with the internal retry handling these errors silently up to 4 times. Additionally if multiple failed returns of structured JSON occurs, a fallback using the repair_json library is engaged to salvage the json from the raw text JSON dump. A general retry mechanism via tenacity otherwise handles general API and rate-limit errors.

--- fast_graphrag/_llm/__init__.py
+++ fast_graphrag/_llm/__init__.py
@@ -6,8 +6,11 @@ __all__ = [
     "format_and_send_prompt",
     "OpenAIEmbeddingService",
     "OpenAILLMService",
+    "GeminiLLMService",
+    "GeminiEmbeddingService",
 ]
 
 from ._base import BaseEmbeddingService, BaseLLMService, format_and_send_prompt
 from ._default import DefaultEmbeddingService, DefaultLLMService
 from ._llm_openai import OpenAIEmbeddingService, OpenAILLMService
+from ._llm_genai import GeminiLLMService, GeminiEmbeddingService

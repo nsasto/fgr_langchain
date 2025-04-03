ed3352cf80d02a3ec5222b62dad4b89da80d8bbb
Nathan Sasto <nsasto@gmail.com>
2025-04-03 16:01:43 +0100

    initial cleanup in prep for refactor

--- benchmarks/create_db.bat
+++
@@ -1,8 +0,0 @@
-:: 2wikimultihopqa benchmark
-:: Creating databases
-python graph_benchmark.py -n 51 -c
-python graph_benchmark.py -n 101 -c
-
-:: Evaluation (create reports)
-python graph_benchmark.py -n 51 -b
-python graph_benchmark.py -n 101 -b
